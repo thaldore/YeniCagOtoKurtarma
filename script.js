94a1f4ceb67b2e56966e92710d6fc4b3fe615ac2
Tolga Çınar <tolgacinar4299@gmail.com>
2024-11-23 15:26:18 +0300

    İlk yükleme

---
+++ script.js
@@ -0,0 +1,4 @@
+// Hamburger menü simgesine tıklandığında menüyü göster
+document.querySelector(".hamburger-menu").addEventListener("click", function() {
+    document.querySelector(".menu").classList.toggle("show");
+});
