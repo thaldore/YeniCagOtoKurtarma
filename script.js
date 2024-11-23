9deb919a608d76f8dc0082fa2166defc6a274d04
Tolga Çınar <tolgacinar4299@gmail.com>
2024-11-23 19:18:32 +0300

    proje 1

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.querySelector(".hamburger-menu").addEventListener("click", function() {
     document.querySelector(".menu").classList.toggle("show");
 });
+
+function gosterDetay(hizmet, resim, aciklama) {
+    const detayBolumu = document.getElementById('hizmet-detay');
+    document.getElementById('detay-resim').src = resim;
+    document.getElementById('detay-aciklama').textContent = aciklama;
+    detayBolumu.classList.add('show');
+}
+
+function kapatDetay() {
+    const detayBolumu = document.getElementById('hizmet-detay');
+    detayBolumu.classList.remove('show');
+}
