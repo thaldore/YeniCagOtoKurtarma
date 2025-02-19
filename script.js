b861473b43602f7c1cd7d43e9e6855df049a6538
Tolga Çınar <tolgacinar4299@gmail.com>
2025-02-19 23:14:44 +0300

    Html Css JS Update 7

--- script.js
+++ script.js
@@ -2,27 +2,45 @@
 const slider = document.querySelector('.slider');
 const prevButton = document.querySelector('.control-prev');
 const nextButton = document.querySelector('.control-next');
+const indexContainer = document.querySelector('.index-container');
 let currentSlide = 0;
+
+// Slider resimlerini yükle
+const sliderItems = document.querySelectorAll('.slider-item');
+sliderItems.forEach((item, index) => {
+    item.style.backgroundImage = `url(${item.getAttribute('data-img-url')})`;
+    const indexButton = document.createElement('button');
+    indexButton.addEventListener('click', () => showSlide(index));
+    indexContainer.appendChild(indexButton);
+});
 
 function showSlide(index) {
     slider.style.transform = `translateX(-${index * 100}%)`;
     currentSlide = index;
+    updateIndexButtons();
 }
 
 prevButton.addEventListener('click', () => {
-    currentSlide = (currentSlide - 1 + 3) % 3; // 3 resim olduğu için
+    currentSlide = (currentSlide - 1 + sliderItems.length) % sliderItems.length;
     showSlide(currentSlide);
 });
 
 nextButton.addEventListener('click', () => {
-    currentSlide = (currentSlide + 1) % 3; // 3 resim olduğu için
+    currentSlide = (currentSlide + 1) % sliderItems.length;
     showSlide(currentSlide);
 });
 
 setInterval(() => {
-    currentSlide = (currentSlide + 1) % 3; // 3 resim olduğu için
+    currentSlide = (currentSlide + 1) % sliderItems.length;
     showSlide(currentSlide);
 }, 7000); // 7 saniyede bir geçiş
+
+function updateIndexButtons() {
+    const buttons = indexContainer.querySelectorAll('button');
+    buttons.forEach((button, index) => {
+        button.classList.toggle('active', index === currentSlide);
+    });
+}
 
 // Hizmet Alanlarını Kaydırma
 const serviceWrapper = document.querySelector('.service-wrapper');
